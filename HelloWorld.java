73bb78fdf9a44f40affdbda282b435d5aa973498
Shitalkumari8-cloud <singhshital9876@gmail.com>
2026-08-25 22:46:37 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,3 +4,17 @@ System.out.println("hello world");
 }
 }
 */
+//Sum of Number//
+import java.util.scanner;
+public static SumOfNumbers{
+public static void main(String[]args){
+Scanner sc = new Scanner(System.in);
+System.out.printl("Enter firsr number":);
+int a = sc.nextInt();
+System lut.print("Enter second number":);
+int b = sc.nextInt();
+Sum = a+b;
+System.out.println("Sum = " +Sum);
+}
+}
+
