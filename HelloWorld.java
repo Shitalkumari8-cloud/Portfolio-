1bd8bb6d755f1f59eb2a572001b8cd76c35f193b
Shitalkumari8-cloud <singhshital9876@gmail.com>
2026-08-25 23:13:40 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -58,6 +58,19 @@ System.out.println(c + " is largest");
 }
 */
 
+//Even or Odd//
+Import java.util.scanner;
+public class EvenOrOdd{
+public static void main(String[]args){
+Scanner sc = new scanner(System.in);
+int n = sc.nextInt();
+if( n % 2 ==0){
+System.out.print("Even");
+}else{
+System.out.print("Odd");
+}
+}
+}
 
 
 
