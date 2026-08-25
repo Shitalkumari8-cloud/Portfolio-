e76c6e058aea7e7633ccd223ea286245ec757ee0
Shitalkumari8-cloud <singhshital9876@gmail.com>
2026-08-25 22:57:22 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -20,3 +20,19 @@ System.out.println("Sum = " +Sum);
 */
 
 //Largest of TwoNumbers//
+Import Java.util.scanner;
+public class LargestOfNumber{
+Public static void main(String[]args){
+Scanner sc = new scanner (System.in);
+System.out.print("Enter first number":);
+int a= sc.nextInt();
+System.out.print("Enter second number":):
+int b = sc.nextInt();
+if(a>b){
+system.out.println(a + " is largest")
+} else {
+system.out.println(b + " is largest");
+}
+}
+}
+
