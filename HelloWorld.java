02ab3f4849cb4bc59edef70e9c92f01cd2421a82
Shitalkumari8-cloud <singhshital9876@gmail.com>
2026-08-26 14:36:00 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -123,11 +123,11 @@ public static void main(String[]args){
 Scanner sc = new Scanner(System.in);
 System.out.print("Enter a number");
 int a = sc.nextInt();
-int Sum = 0;
-for (int i=0,i<=n,i++){
-sum = sum+i;
+int sum = 0;
+for (int i=0,i<=a,i++){
+sum = sum + i;
 }
-system.out.println("sum=" + sum);
+System.out.println("sum=" + sum);
 }
 }
 
